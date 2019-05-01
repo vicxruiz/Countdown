35d8ce19832d92c14596eac31862c630dea502db
victor <ruizdvictor@gmail.com>
2019-05-01 12:34:42 -0400

    Updated ui

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CountdownViewController.swift
 //  Countdown
 //
 //  Created by Victor  on 5/1/19.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CountdownViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
 }
+
 
